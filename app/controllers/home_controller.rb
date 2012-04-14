63a419d476901ac6d6f190594f3d2c4d4baaedef
bhennes2 <bhennes2@gmail.com>
2012-04-14 13:22:33 -0500

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+
+	def index
+		@title = "myDash | Home"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mydash::Application.config.secret_token = '66de4f0da52c69fdef8cace1eae89a3c3e753ebe2471dde45d1fbaa7ee4d00203d5dab7ec6b9dc2c1d63b52ef5af7eaf652a210b72ce062521d9c1090d31f976'
